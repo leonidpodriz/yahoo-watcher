c4a2ff9f2fe2922dc12273574c15ee26b07f5285
leonidpodriz <leonidpodriz@gmail.com>
2020-07-24 11:19:51 +0300

    Init

---
+++ main.php
@@ -0,0 +1,91 @@
+<?php
+
+class BaseRSSParser
+{
+    public $rss_url = "https://rss.url/";
+
+    private function get_xml_data($link)
+    {
+        return simplexml_load_file($link);
+    }
+
+    public function xmlToArray ( $xmlObject, $out = array () )
+    {
+        foreach ( (array) $xmlObject as $index => $node )
+            $out[$index] = ( is_object ( $node ) ) ? $this -> xmlToArray ( $node ) : $node;
+
+        return $out;
+    }
+
+    private function _get_item_content($link) {
+        $content = $this -> get_xml_data($link);
+        return $link;
+    }
+
+    private function _process_item($item) {
+        $item = $this -> xmlToArray($item);
+        $item["content"] = $this -> _get_item_content($item["link"]);
+        return $item;
+    }
+
+    public function get_rss_posts()
+    {
+        $posts = array();
+        $rss_posts = $this->get_xml_data($this->rss_url)->{"channel"}->item;
+        foreach ($rss_posts as $rss_post) {
+            array_push($posts, $rss_post);
+        }
+        return array_map(array($this, "_process_item"), $posts);
+    }
+}
+
+class RSSWordPressPostsCreator extends BaseRSSParser
+{
+    public $post_type = "post";
+
+    private function convert_post_data($post_data)
+    {
+        return array(
+            'post_title' => $post_data->title->__toString(),
+            'post_content' => $post_data->description->__toString(),
+            'post_type' => $this->post_type,
+            'post_status' => 'publish',
+            'post_author' => 1,
+        );
+    }
+
+    private function publish_post_if_need($wp_post) {
+        $found_post = null;
+
+        if ( $posts = get_posts( array(
+            'post_title' => $wp_post->post_title,
+            'post_type' => $this -> post_type,
+            'posts_per_page' => 1,
+        ) ) ) $found_post = $posts[0];
+
+        if ( !is_null( $found_post ) ){
+            wp_insert_post($wp_post);
+        }
+    }
+
+    public function createNewPosts()
+    {
+        $rss_posts = $this->get_rss_posts();
+        $wp_posts = array_map(array($this, "convert_post_data"), $rss_posts);
+        array_map(array($this, "publish_post_if_need"), $wp_posts);
+    }
+}
+
+class FinanceRSSParser extends RSSWordPressPostsCreator
+{
+    public $rss_url = "https://finance.yahoo.com/rss/";
+}
+
+class EntertainmentRSSParser extends BaseRSSParser
+{
+    public $rss_url = "https://news.yahoo.com/rss/entertainment";
+}
+
+$parser = new FinanceRSSParser;
+$posts = $parser -> get_rss_posts();
+print_r($posts);
---
+++ yahoo-watcher.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+Plugin Name: Yahoo Watcher
+Description: Yahoo post watcher
+Version: 1.0
+Author: leonidpodriz
+Author URI: https://leonidpodriz.github.io/
+*/
+
+require "main.php";
+
+function yahoo_watcher_setup()
+{
+    register_post_type('finance', array(
+        'label' => 'Finance',
+        'labels' => array(
+            'name' => 'Finance',
+            'singular_name' => 'Finance',
+            'menu_name' => 'Finance',
+            'all_items' => 'All finance posts',
+            'add_new' => 'Add finance post',
+            'add_new_item' => 'Add new finance post',
+            'edit' => 'Edit',
+            'edit_item' => 'Edit finance post',
+            'new_item' => 'New finance post',
+        ),
+        'description' => 'Posts from Yahoo.com',
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_rest' => false,
+        'rest_base' => '',
+        'show_in_menu' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'post',
+        'map_meta_cap' => true,
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'finance/%finance%', 'with_front' => false, 'pages' => false, 'feeds' => false, 'feed' => false),
+        'has_archive' => 'finances',
+        'query_var' => true,
+        'supports' => array('title', 'editor')
+    ));
+
+    register_post_type('entertainment', array(
+        'label' => 'Entertainment',
+        'labels' => array(
+            'name' => 'Entertainment',
+            'singular_name' => 'Entertainment',
+            'menu_name' => 'Entertainment',
+            'all_items' => 'All entertainment posts',
+            'add_new' => 'Add entertainment post',
+            'add_new_item' => 'Add new entertainment post',
+            'edit' => 'Edit',
+            'edit_item' => 'Edit entertainment post',
+            'new_item' => 'New entertainment post',
+        ),
+        'description' => 'Posts from Yahoo.com',
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_rest' => false,
+        'rest_base' => '',
+        'show_in_menu' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'post',
+        'map_meta_cap' => true,
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'entertainment/%entertainment%', 'with_front' => false, 'pages' => false, 'feeds' => false, 'feed' => false),
+        'has_archive' => 'entertainment',
+        'query_var' => true,
+        'supports' => array('title', 'editor')
+    ));
+
+}
+
+add_action('init', 'yahoo_watcher_setup');
+
+function yahoo_watcher_activate()
+{
+    yahoo_watcher_setup();
+    flush_rewrite_rules();
+
+    wp_schedule_single_event( time() + 10, 'update_yahoo_action_hook' );
+}
+
+add_action( 'update_yahoo_action_hook', 'updateYahooPosts' );
+
+function updateYahooPosts() {
+
+    $post_creator = new FinanceRSSParser;
+    $post_creator -> createNewPosts();
+
+    wp_schedule_single_event( time() + 10, 'update_yahoo_action_hook' );
+}
+
+register_activation_hook(__FILE__, 'yahoo_watcher_activate');
+
+function yahoo_watcher_deactivate()
+{
+    unregister_post_type('book');
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook(__FILE__, 'yahoo_watcher_deactivate');
