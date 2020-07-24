e435b6a1b03bc472c1ea98e3e8f4f39a979ee620
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-24 09:09:56 +0300

    Refactor code

--- posts_creator.php
+++ posts_creator.php
@@ -9,23 +9,12 @@ class BaseRSSParser
         return simplexml_load_file($link);
     }
 
-    public function xmlToArray ( $xmlObject, $out = array () )
+    public function xmlToArray($xmlObject, $out = array())
     {
-        foreach ( (array) $xmlObject as $index => $node )
-            $out[$index] = ( is_object ( $node ) ) ? $this -> xmlToArray ( $node ) : $node;
+        foreach ((array)$xmlObject as $index => $node)
+            $out[$index] = (is_object($node)) ? $this->xmlToArray($node) : $node;
 
         return $out;
-    }
-
-    private function _get_item_content($link) {
-        $content = $this -> get_xml_data($link);
-        return $link;
-    }
-
-    private function _process_item($item) {
-        $item = $this -> xmlToArray($item);
-        $item["content"] = $this -> _get_item_content($item["link"]);
-        return $item;
     }
 
     public function get_rss_posts()
@@ -35,7 +24,7 @@ class BaseRSSParser
         foreach ($rss_posts as $rss_post) {
             array_push($posts, $rss_post);
         }
-        // return array_map(array($this, "_process_item"), $posts);
+
         return $posts;
     }
 }
@@ -47,24 +36,25 @@ class RSSWordPressPostsCreator extends BaseRSSParser
     private function convert_post_data($post_data)
     {
         return array(
-            'post_title' => $post_data->title->__toString(),
-            'post_content' => $post_data->description->__toString(),
+            'post_title' => $post_data->title,
+            'post_content' => $post_data->description,
             'post_type' => $this->post_type,
             'post_status' => 'publish',
             'post_author' => 1,
         );
     }
 
-    private function publish_post_if_need($wp_post) {
+    private function publish_post_if_need($wp_post)
+    {
         $found_post = null;
 
-        if ( $posts = get_posts( array(
+        if ($posts = get_posts(array(
             'post_title' => $wp_post->post_title,
-            'post_type' => $this -> post_type,
+            'post_type' => $this->post_type,
             'posts_per_page' => 1,
-        ) ) ) $found_post = $posts[0];
+        ))) $found_post = $posts[0];
 
-        if ( is_null( $found_post ) ){
+        if ( !is_null($found_post)) {
             wp_insert_post($wp_post);
         }
     }
